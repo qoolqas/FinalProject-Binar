c87dfc40e06ad8e8cc6178aef58d065260b49155
qoolqas <qoolqas@gmail.com>
2023-11-20 19:10:52 +0700

    initial commit

---
+++ app/src/main/java/com/raveendra/finalproject_binar/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.raveendra.finalproject_binar.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.raveendra.finalproject_binar.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
